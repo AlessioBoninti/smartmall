94b533a8989d2ac7243a09e24de87f494b18cd9f
Alessio Boninti <alessio.boninti@studenti.unicam.it>
2026-03-14 15:37:03 +0100

    add spring boot main application class

---
+++ src/main/java/it/smartmall/SmartmallApplication.java
@@ -0,0 +1,13 @@
+package it.smartmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmallApplication.class, args);
+	}
+
+}
