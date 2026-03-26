f9a0fea55efdd80055a737cff7795d52feb9682e
Leonardo Costantini <leonardocostantini@192.168.5.171>
2026-03-26 10:54:10 +0100

    Add Spring Boot test class

---
+++ src/test/java/it/smartmall/SmartmallApplicationTests.java
@@ -0,0 +1,13 @@
+package it.smartmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
